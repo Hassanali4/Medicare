134c1d2c55407e45978cb940d73e6a9ed0605f75
Hassanali4 <89867379+Hassanali4@users.noreply.github.com>
2023-03-17 05:22:56 +0500

    removed

--- med/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-
-
-admin.site.register(Buy)
-admin.site.register(NGO)
-admin.site.register(Contact)
-admin.site.register(CardDetail)
